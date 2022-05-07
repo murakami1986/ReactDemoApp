976cc55f18f7608352d24d8d270b7c6bdef59d05
murakami1986 <90084261+murakami1986@users.noreply.github.com>
2022-05-07 15:48:54 +0900

    first commit

---
+++ section12/src/hooks/useSelectUser.ts
@@ -0,0 +1,22 @@
+import { useCallback, useState } from "react";
+
+import { User } from "../types/api/user";
+
+type Props = {
+  id: number;
+  users: Array<User>;
+  onOpen: () => void;
+};
+
+//選択したユーザー情報を特定しモーダルを表示するカスタムフック
+export const useSelectUser = () => {
+  const [selectedUser, setSlectedUser] = useState<User | null>(null);
+
+  const onSlectUser = useCallback((props: Props) => {
+    const { id, users, onOpen } = props;
+    const targetUser = users.find((user) => user.id === id);
+    setSlectedUser(targetUser ?? null);
+    onOpen();
+  }, []);
+  return { onSlectUser, selectedUser };
+};
