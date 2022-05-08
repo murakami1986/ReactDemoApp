e95690481c6c0e7b4adc8bba861e7b53c345f3f6
murakami1986 <90084261+murakami1986@users.noreply.github.com>
2022-05-08 09:56:14 +0900

    TypeScript

---
+++ TypeScript-practice/src/App.tsx
@@ -0,0 +1,16 @@
+import { Practice1 } from "./practies/Practices1";
+import { Practice2 } from "./practies/Practices2";
+import { Practice3 } from "./practies/Practices3";
+import { Practice4 } from "./practies/Practices4";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
+    </div>
+  );
+}
